cc98d927b0fca22a51bdc41f26cf2dc501d54274
Maxi Lopez <m.lopez@itecriocuarto.org.ar>
2025-11-04 09:33:19 -0300

    Borrar todo el repo

--- schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user_schema import UserSchema, RegisterSchema, LoginSchema
-from .post_schema import PostSchema
-from .comment_schema import CommentSchema
-from .category_schema import CategorySchema
-
